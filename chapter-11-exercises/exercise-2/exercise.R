6f9f394bb5ae6f84771cdf50f291344c86a69635
Peter Lam <someonesbrother@yahoo.com>
2020-01-31 16:29:01 -0800

    Exercise week 4

--- chapter-11-exercises/exercise-2/exercise.R
+++ chapter-11-exercises/exercise-2/exercise.R
@@ -9,19 +9,25 @@ library(fueleconomy)
 
 # Install and load the "dplyr" library
 
+library("dplyr")
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
+
+makes <- select(vehicles, make)
 
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
 
+distinct(makes)
 
 # Filter the data set for vehicles manufactured in 1997
 
+vehicles_1997 <- filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
+
 
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
